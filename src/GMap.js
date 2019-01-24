79388c3d79fed1643b31413e7e60c29f5b7ba2a0
Simon Spozio <simon.spozio@gmail.com>
2019-01-24 19:11:48 +0700

    :sparkles: add simple restaurant markers for bang sue are

--- src/GMap.js
+++ src/GMap.js
@@ -9,13 +9,62 @@ import {
 } from 'native-base'
 
 
+const GOOGLE_PLACES_API_KEY = 'paste-your-key-here'
+
+
 export default class GMap extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isLoading: true,
+      markers: [],
+    }
+  }
+
+  componentDidMount () {
+    this.fetchPlacesData()
+  }
+
+  fetchPlacesData = () => {
+    fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${GOOGLE_PLACES_API_KEY}&types=restaurant&location=13.819792,100.528361&radius=2600`)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({ 
+          isLoading: false,
+          markers: responseJson.results, 
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  renderMarkers = () => {
+    if (this.state.isLoading) return null
+    return this.state.markers.map((marker, index) => {
+      const coords = {
+        latitude: marker.geometry.location.lat,
+        longitude: marker.geometry.location.lng,
+    }
+
+    return (
+        <MapView.Marker
+            key={index}
+            coordinate={coords}
+            title={marker.name}
+            description={marker.vicinity}
+        />
+    )
+    })
+  }
+
   render(){
     return(
       <Container>
         <Header style={styles.header}>
           <Body>
-            <Title>GMap</Title>
+            <Title>GMap: Bang Sue Restaurant</Title>
           </Body>
         </Header>
           <MapView
@@ -26,7 +75,9 @@ export default class GMap extends Component {
               latitudeDelta: 0.056011,
               longitudeDelta: 0.043858,
             }}
-          />
+          >
+            {this.renderMarkers()}
+          </MapView>
       </Container>
     )
   }
